6bc9f40ed3a048c678a5852cc3d39f0d2518492b
Carl Diehl <carl.diehl@web.de>
2024-02-05 12:22:44 +0100

    include lngamma in sfuns.dll

--- exercises/02math/sfuns.cs
+++ exercises/02math/sfuns.cs
@@ -8,7 +8,11 @@ public static class sfuns{
 	double lnfgamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
 		return Exp(lnfgamma);
 }
-
-
+	public static double lngamma(double x){ 
+		if(x<0)return double.NaN;
+		if(x<9)return lngamma(x+1) - Log(x);
+	double lnfgamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
+		return lnfgamma;
+}
 
 }
